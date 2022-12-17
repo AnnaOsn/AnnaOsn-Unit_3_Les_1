c551a9fee4a993f4b2aa03cda8adbcc0d1b82963
Anna_Osn <Anya_kas@live.ru>
2022-12-17 20:10:36 +0300

    add minMax

---
+++ Examples/Seminar01_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b) 
+{
+    Console.Write("max: ");
+Console.WriteLine(a);
+Console.Write("min: ");
+Console.WriteLine(b);
+}
+else
+{
+    Console.Write("max: ");
+Console.WriteLine(b);
+Console.Write("min: ");
+Console.WriteLine(a);
+
+}
